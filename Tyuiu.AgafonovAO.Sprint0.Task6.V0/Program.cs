283ceeb92627896f42075e7daa05e2529345ebcb
ANYA1GOD <bendyyt697@gmail.com>
2026-09-13 23:57:03 +0500

    Task6

---
+++ Tyuiu.AgafonovAO.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AgafonovAO.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.AgafonovAO.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = { 1, 2, 3, 4, 5 };
+            
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            
+            Console.ReadKey();
+        
+        }
+    }
+}
