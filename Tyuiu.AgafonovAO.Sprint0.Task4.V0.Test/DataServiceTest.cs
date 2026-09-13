829d0a306280c1c26040828cc31a89f8050a90f6
ANYA1GOD <bendyyt697@gmail.com>
2026-09-13 20:41:20 +0500

    Task4

---
+++ Tyuiu.AgafonovAO.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AgafonovAO.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.AgafonovAO.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+          
+        }
+
+        [TestMethod]
+        public void CheckSubtractionlValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationlValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+
+        }
+
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+
+        }
+    }
+}
