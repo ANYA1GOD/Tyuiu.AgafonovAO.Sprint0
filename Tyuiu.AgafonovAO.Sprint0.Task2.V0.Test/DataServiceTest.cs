d8faf2351daef130f0a441f61241dbcd94ecf885
ANYA1GOD <bendyyt697@gmail.com>
2026-09-13 19:40:12 +0500

    Task2

---
+++ Tyuiu.AgafonovAO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AgafonovAO.Sprint0.Task2.V0.lib;
+
+namespace Tyuiu.AgafonovAO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Артур";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Артур", res);
+        }
+    }
+}
---
+++ Tyuiu.AgafonovAO.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AgafonovAO.Sprint0.Task2.V0.lib;
+
+namespace Tyuiu.AgafonovAO.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Артур"));
+            Console.ReadKey();
+           
+        }
+    }
+}
