84623bdfe28728c496ce82de8dfbcc283372bfd1
ANYA1GOD <bendyyt697@gmail.com>
2026-09-14 00:57:39 +0500

    Task7

---
+++ Tyuiu.AgafonovAO.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.AgafonovAO.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numeTwo) 
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++) 
+            {
+                resultArray[i] = numOne[i] + numeTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
